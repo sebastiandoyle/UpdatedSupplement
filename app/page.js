1c15a6f899c56c896a86abc42849fbea3ef91c40
sebastiandoyle <157081826+sebastiandoyle@users.noreply.github.com>
2024-10-25 19:37:30 +1100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client';
+
 import React, { useState, useEffect, useRef } from 'react';
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
